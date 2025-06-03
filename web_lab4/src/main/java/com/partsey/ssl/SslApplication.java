6814cd4bb3c5cedfc9fec5a8e793be8adece527c
Partsei Ihor <99366405+asdsadff@users.noreply.github.com>
2025-06-03 19:49:21 +0300

    Add files via upload

---
+++ web_lab4/src/main/java/com/partsey/ssl/SslApplication.java
@@ -0,0 +1,14 @@
+package com.partsey.ssl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslApplication {
+
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("java.home"));
+		SpringApplication.run(SslApplication.class, args);
+	}
+
+}
